0d75a75351cafa5eb7359ce9efc841603036be1f
rfogari <fogari10@gmail.com>
2018-06-08 07:00:54 -0400

    Updates to javascript, html and css.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,11 +18,14 @@ var currentQuetion;
 //Game questions array
 var gameQuestions = [{
        
-    question: "In which state is it illegal to eat an orange in a bathub?",
+    question: "The first baseball game was played in what city?",
+    answers: ["Atlanta, Georgia", "Brooklyn, New York", "Chicago, Illinois", "Hoboken, New Jersey"],
+    correctAnswer: "Hoboken, New Jersey",
 
-    answers: ["California", "New York", "Florida", "Vermont"],
-    correctAnswer: "California",
-}, {
+}, 
+
+{
+
     question: "What is the largest two digit prime number?",
     answers: ["95", "97", "89", "93"],
     correctAnswer: "97",
@@ -51,17 +54,25 @@ var gameQuestions = [{
     question: "What letter and number is given to the leading Western Economic powers?",
     answers: ["E3", "M6", "E8", "G8"],
     correctAnswer: "G8",
-}
-]
+},
+];
 
 
 //array length needed for for loop later on in code to cycle through all questions being asked.
+
+
 var questionsLength = gameQuestions.length;
+
+console.log(questionsLength);
 
 
 $(document).ready(function(){
     $('.button-start').text("Click Here to Start!");
     $('.time-display').hide();
+    $('.info-container').hide();
+    $('.question').hide();
+    $('.answer-buttons').hide();
+    $('.clock').hide();
     $('.button-start').on("click", function (){
     $('.instructions').hide()
         displayQuestions();
@@ -85,10 +96,12 @@ function correctAnswer (){
     clearInterval(intervalId);
 
     if (questionCounter == questionsLength){
-        setTimeout(endScreen, 5000);
+        setTimeout(endScreen, 4000);
     } else {
-        setTimeout(displayQuestions, 2000);
+        setTimeout(displayQuestions, 5000);
     }
+
+    console.log(questionCounter);
 };
 
 
@@ -105,10 +118,12 @@ function wrongAnswer (){
     clearInterval(intervalId);
 
     if (questionCounter == questionsLength){
-        setTimeout(endScreen, 5000);
+        setTimeout(endScreen, 2000);
     } else {
         setTimeout(displayQuestions, 2000);
     }
+
+    console.log(questionCounter);
 };
 
 
@@ -117,21 +132,25 @@ function timesUp () {
     $('.button').off("click");
     $('.question').text("Time's Up! Game Over!")
     $('.answer-buttons').hide();
+    $('.clock').hide();
     questionCounter++;
     blank++;
     clearInterval(intervalId);
+    
     endScreen();
+
+    console.log(questionCounter);
 
 };
 
 
 //function for end screen - this will update player on game stats and option to reset game.
-function endScreen() {
+function endScreen(){
     $('.results-buttons').show();
     $('.button-results').text("Game Over!");
     $('.win').text("Correct Answers:" + correct);
     $('.wrong').text("Wrong Answers:" + wrong);
-    $('.draw').text("Unanswered Questions" + blank);
+    //$('.draw').text("Unanswered Questions" + blank);
     $('.time-display').hide();
 
     //reset and restart
@@ -141,13 +160,16 @@ function endScreen() {
 
 //reset the game - reset the counter and remove any previous scores
 function gameReset(){
-    questionCounter= 0;
-    correct =0;
-    wrong=0;
-    blank=0;
+    questionCounter = 0;
+    correct = 0;
+    wrong = 0;
+    blank = 0;
 
 
-    return questionCounter, correct, wrong, blank
+    return questionCounter;
+    return correct;
+    return wrong;
+    return blank;
 
     $('.results-buttons').hide();
 
@@ -156,9 +178,11 @@ function gameReset(){
 
 //function for displaying game questions
 function displayQuestions(){
-    $('.button-start').hide();
+    $('.start').hide();
     $('.time-display').show();
-    
+    $('.clock').show()
+    $('.question').show();
+    $('.info-container').show();
 
     //question timer
     var timer = 10;
@@ -166,7 +190,7 @@ function displayQuestions(){
     function decrement() {
         timer--;
         $(".timer").html(timer);
-        if (timer ===0) {
+        if (timer == 0) {
             timesUp();
         };
     };
@@ -188,14 +212,14 @@ function displayQuestions(){
         if (        
             $(this).text() === gameQuestions[questionCounter].correctAnswer){
             
-                console.log(gameQuestions[questionCounter].correctAnswer)
-            
+                       
                 correctAnswer();
-                console.log(correctAnswer());
-        
+                console.log(this);
+                        
             } else {
-        
+                
                 wrongAnswer();
+                console.log(this);
         }    
     
     
